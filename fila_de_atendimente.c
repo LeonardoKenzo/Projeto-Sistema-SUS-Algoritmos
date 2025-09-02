5b2d6873d32beb22f5512bcafcd3d1da740977a4
pedroYamacita <pedro.teidi@gmail.com>
2025-09-02 17:25:40 -0300

    bugfix/Fila-De-Atendimento-Encadeada
    Agora fiz encadeadamente hahahahhahhahaha

--- fila_de_atendimente.c
+++ fila_de_atendimente.c
@@ -5,13 +5,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+struct no_fila_
+{
+    PACIENTE *paciente;
+    NO_FILA *proximo;
+};
+
+
 struct fila_de_atendimento_
 {
-    PACIENTE **pacientes;
-    int capacidade;
-    int inicio;
-    int fim;
-    Fila_De_Atendimento* proximo;
+    NO_FILA *inicio;
+    NO_FILA *fim;
 };
 
 Fila_De_Atendimento *Fila_Criar(int capacidade){
@@ -19,15 +23,9 @@ Fila_De_Atendimento *Fila_Criar(int capacidade){
     if (fila == NULL) {
         return NULL;
     }
-    fila->pacientes = (PACIENTE **)malloc(capacidade * sizeof(PACIENTE *));
-    if (fila->pacientes == NULL) {
-        free(fila);
-        return NULL;
-    }
-    fila->capacidade = capacidade;
-    fila->inicio = 0;
-    fila->fim = 0;
-    fila->proximo = NULL;
+
+    fila->inicio = NULL;
+    fila->fim = NULL;
     return fila;
 }
 
@@ -35,41 +33,63 @@ bool Fila_Vazia(Fila_De_Atendimento *fila) {
     if (fila == NULL) {
         return true;
     }
-    return fila->inicio == fila->fim;
+    return fila->inicio == NULL;
 }
 
 bool Fila_Cheia(Fila_De_Atendimento *fila) {
-    if (fila == NULL) {
-        return false;
+    //só é verdadeiro se faltar memória
+    return false;
     }
-    return (fila->fim + 1) % fila->capacidade == fila->inicio;
-}
 
 bool Fila_Inserir(Fila_De_Atendimento *fila, PACIENTE *paciente) {
     if ((fila == NULL && !Fila_Cheia(fila)) || paciente == NULL) {
         return false;
     }
+    NO_FILA *novo = (NO_FILA *)malloc(sizeof(NO_FILA));
+    //verifica se memória foi alocada corretamente
+    if(novo == NULL){
+        return false;
+    }
 
-    fila->pacientes[fila->fim] = paciente;
-    fila->fim = (fila->fim + 1) % fila->capacidade;
-    fila->proximo = fila->pacientes[fila->fim];
+    novo->paciente = paciente;
+    novo->proximo = NULL;
+    //fila esta vazia, ngm aponta pra ele
+    if(fila->inicio == NULL){
+        fila->inicio = novo;
+    } 
+    //fila ja tem pelo menos um nó anterior q vai apontar para o novo
+    else{
+        fila->fim->proximo = novo;
+    }
+    fila->fim = novo;
     return true;
 }
 
 PACIENTE *Fila_Remover(Fila_De_Atendimento *fila) {
-    if (fila == NULL && !Fila_Vazia(fila)) {
+    if(fila == NULL || fila->inicio == NULL){
         return NULL;
     }
-    PACIENTE *pacienteRemovido = fila->pacientes[fila->inicio];
-    fila->inicio = (fila->inicio + 1) % fila->capacidade;
+    NO_FILA *remover = fila->inicio;
+    PACIENTE *pacienteRemovido = remover->paciente;
+    
+    fila->inicio = remover->proximo;
+    ///se nao houver mais elementos, tanto inicio quanto fim apontam para NULL indicando fila vazia
+    if(fila->inicio == NULL){
+        fila->fim = NULL;
+    }
+    free(remover);
     return pacienteRemovido;
 }
-
 void Fila_Liberar(Fila_De_Atendimento **fila) {
     if (fila == NULL || *fila == NULL) {
         return;
     }
-    free((*fila)->pacientes);
+    NO_FILA *atual = (*fila)->inicio;
+    while (atual != NULL){
+        NO_FILA *proximo = atual->proximo;
+        free(atual);
+        atual = proximo;
+    }
     free(*fila);
-    *fila = NULL;
+    *fila=NULL;
 }
--- fila_de_atendimente.h
+++ fila_de_atendimente.h
@@ -2,6 +2,7 @@
     #define FILA_DE_ATENDIMENTO_H
     #include "paciente.h"
 
+    typedef struct no_fila_ NO_FILA;
     typedef struct fila_de_atendimento_ Fila_De_Atendimento;
 
     Fila_De_Atendimento *Fila_Criar(int capacidade);
